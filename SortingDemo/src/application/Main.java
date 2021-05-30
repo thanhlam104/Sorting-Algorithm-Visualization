9188a48584e0b449a901c72d38ca95a257f85ecb
quangtung268 <72648545+quangtung268@users.noreply.github.com>
2021-05-30 21:37:44 +0700

    Main menu

---
+++ SortingDemo/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	private Stage stage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			stage = primaryStage;
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MainMenu.fxml"));
+			Scene scene = new Scene(root);
+			stage.setTitle("Sorting Algorithm");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
