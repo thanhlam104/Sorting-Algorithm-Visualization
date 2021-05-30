8619d4305304ca71f491f7a5fc562338e391e900
quangtung268 <72648545+quangtung268@users.noreply.github.com>
2021-05-30 21:34:30 +0700

    insertion sort + quick sort

---
+++ SortingDemo/src/algorithm/QuickSort.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import element.Element;
+import javafx.animation.Transition;
+import javafx.scene.paint.Color;
+
+public class QuickSort extends GeneralSort{
+	private Color StartColor = Color.GRAY;
+	private Color CompareColor = Color.PLUM;
+	private Color SortedColor = Color.LIGHTSTEELBLUE;
+	
+	private int partition(Element[] arr, int l, int h) {
+		Element pivot = arr[h];
+		colorElements(arr, CompareColor, h);
+		int i = l - 1;
+		for (int j = l; j < h; j++) {
+			colorElements(arr, CompareColor, j);
+			if (arr[j].getValue() < pivot.getValue()) {
+				i++;
+				swap(arr, i, j);
+				colorElements(arr, StartColor, i);
+			}
+			else
+				colorElements(arr, StartColor, j);
+		}			
+		swap(arr, h, i+1);
+		colorElements(arr, SortedColor, i+1);
+	return (i+1);	
+	}
+	private void quickSort(Element[] arr, int l, int h) {
+		if (l<h) {
+			int p = partition(arr, l, h);
+			quickSort(arr, l, p-1);
+			quickSort(arr, p+1, h);
+		}
+	}
+
+	@Override
+	public Transition[] startSort(Element[] arr) {
+		quickSort(arr, 0, arr.length-1);
+		colorArray(arr, SortedColor);
+		return this.transitions;
+	}
+
+}
